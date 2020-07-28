688d8164143a37e7d202176d434303b99f37730f
sounmind <sounmind777@gmail.com>
2020-07-28 13:53:41 +0900

    입력값에 따른 무한루프 예외처리, UI 개선
    미션 3번에서, scanf 예외 처리를 해봤습니다. 하지만 남은 문제들이 있습니다.
    예외처리방법은
    입력을 숫자가 아니라 문자열로 받은 다음, atoi()와 strlen()으로 문자열이 숫자인지, 문자열의 길이가 얼마인지 
    검사합니다.
    하지만 atoi()의 경우
    문자열의 처음부터 숫자가 끝날 때까지를 int형으로 반환하기 때문에 엄밀히 말하면 숫자가 아닌, "123abc"도 "123"를 
    반환한다는 문제가 있습니다.
    문자열이 처음부터 숫자가 아니면 0을 반환하기 때문에 정말로 0을 입력했을 경우와 문자열을 입력했을 경우를 구분하지 못하는 문제가 
    있습니다.

--- week3/w3-3.c
+++ week3/w3-3.c
@@ -1,6 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define QUEUE_LENGTH 10 + 1 // 배열 길이가 N이라면, N-1개 데이터가 채워졌을 때 꽉찬 것으로 간주
+
 /*
 개선된 원형 큐
 큐가 텅 비었을 때
@@ -12,6 +17,7 @@
 큐가 꽉 찼을 때
     rear가 가리키는 위치의 다음 위치를 front가 가리킨다.
 */
+
 typedef struct circularQueue // 원형 큐 자료형 구조체 선언
 {
     int front; // 큐의 머리
@@ -27,26 +33,39 @@ void pop(Queue* pointerQueue);
 void display(Queue* pointerQueue);
 
 int main(void) {
+
     // Queue 생성 및 초기화
     Queue queue;
     queueInit(&queue);
-    int order;
-    int inputContents;
-    int count = 0; // (임시) 문자열 입력 시 걸리는 무한루프 빠져나오기
+
+
+    char order[100];
+    char inputContents[100];
+
     while (1)
     {
         // printf("queue front: %i, queue rear: %i \n", queue.front, queue.rear);
         printf(">>>> 명령 번호를 입력하세요.\n");
         printf(">>>> add(1), pop(2), display(3), quit(4) : ");
-        scanf("%d", &order);
-        switch (order) {
+        scanf("%s", &order);
+
+        if(strlen(order) > 1) { printf("\n>>>> 명령이 너무 깁니다!\n\n"); continue; }                   // 입력 문자가 두 자리 이상이면 continue;
+        if (atoi(order) == 0) { printf("\n>>>> 명령이 해당 숫자(1~4)가 아닙니다!\n\n"); continue; }    // 입력 문자가 숫자가 아니면 continue;
+                                                                                               // 예외1 -> 0을 입력하려 했을 때, 명령이 숫자가 아닌지, 진짜 0을 입력하려 했는지 구분할 수 없다.
+
+
+        switch (atoi(order)) {
         case 1: // add
         {
-            printf(">>>> 추가할 내용을 입력하세요 : ");
-            scanf("%d", &inputContents);
-            add(&queue, inputContents);
+            printf(">>>> 추가할 번호(0 제외)를 입력하세요:     ");
+            scanf("%s", inputContents);
+
+            if (atoi(inputContents) == 0) { printf("\n>>>> 숫자가 아닙니다!\n\n"); continue; } // 예외2 -> atoi()의 경우 첫째 자리부터 숫자 부분만 추출하기 때문에 "132abc123"를 입력 받았을 경우, 132로 입력된다.
+
+            add(&queue, atoi(inputContents));
             break;
         }
+
         case 2: // pop
         {
             pop(&queue);
@@ -59,78 +78,88 @@ int main(void) {
         }
         case 4: // quit
         {
-            printf(">>>> 프로그램을 종료합니다.\n");
+            printf("\n>>>> 프로그램을 종료합니다.\n");
             return 0;
         }
         default:
         {
-            printf(">>>> 다시 제대로 명령 번호를 입력해주세요.\n");
+            printf("\n>>>> 다시 제대로 명령 번호를 입력해주세요.\n\n");
             break;
         }
         }
-        count++;
-        if (count == 100) { return -1; } // (임시) 문자열 입력 시 걸리는 무한루프 빠져나오기
+
     }
+
     return 0;
 }
+
+
+
 void queueInit(Queue* pointerQueue)
 {
     pointerQueue->front = 0;
     pointerQueue->rear = 0;
 }
+
 int isQueueEmpty(Queue* pointerQueue)
 {
     if (pointerQueue->front == pointerQueue->rear)                          // 큐가 텅 비었다 == front와 rear가 동일한 위치를 가리킨다
-    {
-        return 1; // True 비었다
-    }
+    { return 1; }
     else
-    {
-        return 0; // False
-    }
+    { return 0; }
 }
+
 int queueNextPositionIndex(int position)
 {
     if (position == QUEUE_LENGTH - 1)                                       // 위치가 배열의 마지막 요소의 인덱스 값이라면
-        return 0;
+    { return 0; }
     else
-        return position + 1;
+    { return position + 1; }
 }
+
 void add(Queue* pointerQueue, int data)
 {
     if (queueNextPositionIndex(pointerQueue->rear) == pointerQueue->front)  // 큐가 꽉 찼다면, (rear가 가리키는 곳의 다음 위치가 front가 가리키는 곳)
     {
-        printf("큐가 꽉 찼습니다!\n");
+        printf("\n큐가 꽉 찼습니다!\n");
         return;
     }
     pointerQueue->rear = queueNextPositionIndex(pointerQueue->rear);        // rear를 한 칸 이동
     pointerQueue->queueArray[pointerQueue->rear] = data;                    // rear가 가리키는 곳에 데이터 저장
-    printf("정상 입력되었습니다...\n");
+    printf("\n정상 입력되었습니다...\n");
     return;
 }
+
 void pop(Queue* pointerQueue)
 {
     if (isQueueEmpty(pointerQueue))
     {
-        printf("큐가 텅 비었습니다!\n");
+        printf("\n큐가 텅 비었습니다!\n");
         return;
     }
+
     pointerQueue->front = queueNextPositionIndex(pointerQueue->front);                // front를 한 칸 이동
     printf(">>>> pop 실행: %d\n", pointerQueue->queueArray[pointerQueue->front]);     // front가 가리키는 데이터 반환
+
 }
+
 void display(Queue* pointerQueue)
 {
     if (isQueueEmpty(pointerQueue))
     {
-        printf("큐가 텅 비었습니다!\n");
+        printf("\n큐가 텅 비었습니다!\n");
         return;
     }
-    printf("display executed: \n");
+
+    printf("\n>>>>>>>> display executed <<<<<<<<\n");
     int i = pointerQueue->front;
+
     while (1)
     {
         i = queueNextPositionIndex(i); // 다음 위치로 이동
-        printf("index= %d, value= %d \n", i, pointerQueue->queueArray[i]); // 해당 위치에 있는 값 출력
+
+        printf(" index= %d, value= %d \n", i, pointerQueue->queueArray[i]); // 해당 위치에 있는 값 출력
+
         if (pointerQueue->rear == i) // 해당 위치가 rear가 가리키는 위치면 함수 종료
         {
             return;
